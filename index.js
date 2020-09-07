e8dd4c0ad6bb3cab3a576b23b5926ed67a120935
seoyeon1 <seoyeonma1209@gmail.com>
2020-09-07 22:00:07 +0900

    JS로 카운터 기능 구현

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const num = document.getElementById('num')
 const inc = document.getElementById('inc')
 const dec = document.getElementById('dec')
 
+// id가 없을 경우 -> querySelectorAll 이용
+// const btns = document.querySelectorAll('button')
+// const [inc, dec] = btns
+
+
 // 해당 값을 콘솔에 출력
-console.log(num.innerText)//해당 태그 안에 있는 텍스트 읽기
-console.log(inc.offsetTop)//top값
-console.log(dec.id)//해당 DOM의 id 조회
+inc.onclick = () => {
+    const current = parseInt(num.innerText, 10)//문자열->숫자(바꿀 문자열,n진수)
+    num.innerText = current + 1
+}// 클릭 이벤트 설정
+
+dec.onclick = () => {
+    const current = parseInt(num.innerText, 10)//문자열->숫자(바꿀 문자열,n진수)
+    num.innerText = current - 1
+}
+
+// console.log() : console에 내용 출력
+// .id : 해당 DOM의 id 조회
+// .offsetTop : top값
+//.innerText : 해당 태그 안에 있는 텍스트 읽기
